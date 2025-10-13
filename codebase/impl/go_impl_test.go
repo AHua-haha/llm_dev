e6bcc3d58cd2673a3395893a9b488451cb9e8b95
Liu Jia Jun <22421198@zju.edu.cn>
2025-10-13 15:24:21 +0800

    test go parser type to get type declaration position

--- codebase/impl/go_impl_test.go
+++ codebase/impl/go_impl_test.go
@@ -1,6 +1,12 @@
 package impl
 
 import (
+	"fmt"
+	"go/ast"
+	"go/importer"
+	"go/parser"
+	"go/token"
+	"go/types"
 	"testing"
 )
 
@@ -23,4 +29,74 @@ func Test_fileSymExtractOps_extractSymbol(t *testing.T) {
 			tt.fileops.extractSymbol()
 		})
 	}
+}
+
+func Test_goTypeChecker(t *testing.T) {
+	t.Run("test go ast type", func(t *testing.T) {
+		src := `
+package main
+
+import "fmt"
+
+func add(a int, b int) int {
+	return a + b
+}
+type test struct {
+	a int
+	b int
+	c string
+}
+
+func main() {
+	var tt test
+	tt.a = 22
+	var x int = 10
+	y := 20
+	z := "hello"
+	fmt.Println(add(x, y), z)
+}
+`
+
+		fset := token.NewFileSet()
+
+		// Parse the source file
+		file, err := parser.ParseFile(fset, "example.go", src, parser.AllErrors)
+		if err != nil {
+			panic(err)
+		}
+
+		// Prepare type info storage
+		info := &types.Info{
+			Defs:  make(map[*ast.Ident]types.Object),
+			Uses:  make(map[*ast.Ident]types.Object),
+			Types: make(map[ast.Expr]types.TypeAndValue),
+		}
+
+		// Type-check the package
+		conf := types.Config{
+			Importer: importer.Default(),
+			Error: func(err error) {
+				fmt.Printf("type err: %v\n", err)
+			},
+		}
+		_, err = conf.Check("mypkg", fset, []*ast.File{file}, info)
+		if err != nil {
+			panic(err)
+		}
+		for _, obj := range info.Uses {
+			pos := obj.Pos()
+			fmt.Printf("fset.Position(pos): %v\n", fset.Position(pos))
+			switch obj := obj.(type) {
+			case *types.TypeName:
+				fmt.Printf("obj.Name(): %v\n", obj.Name())
+			case *types.Func:
+				fmt.Printf("obj.Name(): %v\n", obj.Name())
+				pkg := obj.Pkg()
+				if pkg == nil {
+					continue
+				}
+				fmt.Printf("pkg.Path(): %v\n", pkg.Path())
+			}
+		}
+	})
 }
