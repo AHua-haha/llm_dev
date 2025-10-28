e318cd416841d4dce8c5f84bc47209bcd11cf6e1
Liu Jia Jun <22421198@zju.edu.cn>
2025-10-28 18:56:56 +0800

    impl gen file map text

---
+++ context/fileContent_test.go
@@ -0,0 +1,20 @@
+package context
+
+import (
+	"bytes"
+	"fmt"
+	"llm_dev/database"
+	"testing"
+)
+
+func TestFileContentCtxMgr_WriteExternalDefs(t *testing.T) {
+	t.Run("test file context mgr", func(t *testing.T) {
+		database.InitDB()
+		defer database.CloseDB()
+		mgr := NewFileCtxMgr("/root/workspace/llm_dev")
+		// mgr.buildCodeBaseCtxop.ExtractDefs()
+		var buf bytes.Buffer
+		mgr.WriteExternalDefs(&buf)
+		fmt.Printf("buf.String(): %v\n", buf.String())
+	})
+}
